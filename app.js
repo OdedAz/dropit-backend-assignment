8ab93c8d870c75ee3f350c8569a85f887645c6e0
oded azoulay <dogshouseisrael@gmail.com>
2022-08-15 20:13:17 +0300

    i edited the structure and added classes and common

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const deliveryController = require('./lib/controllers/deliveryController')
+const GeoServices = require('./lib/services/geoServices')
+const DB = require('./lib/services/db')
+const CourierApi = require('./lib/services/CourierApi')
+
+const config = require('./lib/services/config')
+const port = process.env.port || process.env.PORT || 3000
+
+async function main () {
+  const common = {
+    db: new DB(config.db),
+    geoServices: new GeoServices(config.geo),
+    courierAPI: new CourierApi()
+  }
+
+  const app = express()
+
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+  app.use(cors())
+
+  app.use(deliveryController(common))
+
+  app.listen(port, () => console.log('server is up, listening on port: ' + port))
+}
+
+main()
