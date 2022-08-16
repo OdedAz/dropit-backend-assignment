3cfe51d30768947ffa8af52300fb3a5f1a2c639b
oded azoulay <dogshouseisrael@gmail.com>
2022-08-16 16:20:20 +0300

    saved with prettier

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-const express = require('express')
-require('express-async-errors')
-const logger = require('pino')()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const deliveryController = require('./lib/controllers/deliveryController')
-const GeoServices = require('./lib/services/geoServices')
-const DB = require('./lib/services/db')
-const CourierApi = require('./lib/services/CourierApi')
+const express = require("express");
+require("express-async-errors");
+const logger = require("pino")();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const deliveryController = require("./lib/controllers/deliveryController");
+const GeoServices = require("./lib/services/geoServices");
+const DB = require("./lib/services/db");
+const CourierApi = require("./lib/services/CourierApi");
 
-const config = require('./lib/services/config')
-const port = process.env.port || process.env.PORT || 3000
+const config = require("./lib/services/config");
+const port = process.env.port || process.env.PORT || 3000;
 
-async function main () {
+async function main() {
   const common = {
     db: new DB(config.db),
     geoServices: new GeoServices(config.geo),
-    courierAPI: new CourierApi()
-  }
+    courierAPI: new CourierApi(),
+  };
 
-  const app = express()
+  const app = express();
 
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(bodyParser.json())
-  app.use(cors())
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(cors());
 
-  app.use(deliveryController(common))
+  app.use(deliveryController(common));
 
   app.use((err, req, res, next) => {
-    logger.error(err)
-    res.status(500).end()
-  })
+    logger.error(err);
+    res.status(500).end();
+  });
 
-  app.listen(port, () => console.log('server is up, listening on port: ' + port))
+  app.listen(port, () =>
+    console.log("server is up, listening on port: " + port)
+  );
 }
 
-main()
+main();
